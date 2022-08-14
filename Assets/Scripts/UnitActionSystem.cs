1cfe3625dfeb45190f557089c16ffbf78997dc3a
IrePin <gagistos@gmail.com>
2022-08-15 00:36:37 +0300

    1 commit

---
+++ Assets/Scripts/UnitActionSystem.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitActionSystem : MonoBehaviour
+{
+    public static UnitActionSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedUnitChanged;
+
+    [SerializeField] private Unit selectedUnit;
+    [SerializeField] private LayerMask unitLayerMask;
+
+    private bool isBusy;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one UnitActionSystem! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+
+
+    private void Update()
+    {
+
+        if (isBusy)
+        {
+            return;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (TryHandleUnitSelection()) return;
+
+            GridPosition mouseGridPosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetPosition());
+
+            if (selectedUnit.GetMoveAction().IsValidActionGridPosition(mouseGridPosition))
+            {
+                SetBusy();
+                selectedUnit.GetMoveAction().Move(mouseGridPosition);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            SetBusy();
+            selectedUnit.GetSpinAction().Spin();
+        }
+    }
+
+
+    private void SetBusy()
+    {
+        isBusy = true;
+    }
+
+    private void ClearBusy()
+    {
+        isBusy = false;
+    }
+
+
+    private bool TryHandleUnitSelection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitLayerMask))
+        {
+           if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
+            {
+                SetSelectedUnit(unit);
+                return true;
+            }
+                      
+         }
+        return false;
+    }
+
+    public void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);            
+    }
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
+    }
+
+}
